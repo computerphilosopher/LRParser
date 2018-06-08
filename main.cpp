f803037899eabbb7605703ce7433c5299a2b3754
computerphilosopher <abc3242@gmail.com>
2018-06-09 02:07:16 +0900

    파일분할 성공

--- main.cpp
+++ main.cpp
@@ -4,325 +4,262 @@
 #include <vector>
 #include <sstream>
 #include "enums.h" 
+#include "Class.h" 
 
 using namespace std;
  
-class Action {
+Action::Action(string type, int num) {
+	this->type = type;
+	this->num = num;
+}
 
-private:
-	string type; //shift or reduce;
-	int num; // state or rule number
+string Action::GetType() {
+	return type;
+}
 
-public:
-	
-	Action(string type, int num) {	
-		this->type = type;
-		this->num = num;
+int Action::GetState() {
+	return num;
+}
+
+int Action::GetRuleNumber() {
+	return GetState();
+}
+
+Rule::Rule() {
+
+}
+
+int Rule::GetRHSCount(Action action) {
+
+	int ruleNumber = action.GetRuleNumber();
+
+	switch (ruleNumber) {
+	case 1:
+		return 3;
+		break;
+	case 2:
+		return 1;
+		break;
+	case 3:
+		return 3;
+		break;
+	case 4:
+		return 1;
+		break;
+	case 5:
+		return 3;
+		break;
+	case 6:
+		return 1;
+		break;
+	}
+}
+
+int Rule::GetLHS(Action action) {
+
+	int ruleNumber = action.GetRuleNumber();
+
+	switch (ruleNumber) {
+
+	case 1:
+		return E;
+		break;
+	case 2:
+		return E;
+		break;
+	case 3:
+		return T;
+		break;
+	case 4:
+		return T;
+		break;
+	case 5:
+		return F;
+		break;
+	case 6:
+		return F;
+		break;
+	}
+}
+
+
+
+LRParser::LRParser(string input) {
+	this->input = input;
+}
+
+void LRParser::Reduce() {
+
+}
+
+int LRParser::GetSymbol(char c) {
+	switch (c) {
+	case 'a':
+		return ID;
+		break;
+	case '+':
+		return ADD;
+		break;
+	case '*':
+		return MUL;
+		break;
+	case '(':
+		return LEFT_PAREN;
+		break;
+	case ')':
+		return RIGHT_PAREN;
+		break;
+	case '\0':
+		return END_OF_STRING;
 	}
 
-	string GetType(){
-		return type;
-	}
+	return ERROR;
+}
 
-	int GetState() {
-		return num;
-	}
+string LRParser::PrintStack() {
+	stack <int> temp;
+	temp = parsingStack;
 
-	int GetRuleNumber() {
-		return GetState();
-	}
-};
- 
-class Rule {
+	string ret;
 
-private:
-public:
-	Rule() {
+	while (!temp.empty()) {
+		int k = temp.top();
 
-	}
-
-	static int getRHSCount(Action action){
-
-		int ruleNumber = action.GetRuleNumber();
-
-		switch (ruleNumber) {
-		case 1:
-			return 3;
+		switch (k) {
+		case ID:
+			ret.append("a");
 			break;
-		case 2:
-			return 1;
+		case ADD:
+			ret.append("+");
 			break;
-		case 3:
-			return 3;
+		case MUL:
+			ret.append("*");
 			break;
-		case 4:
-			return 1;
+		case LEFT_PAREN:
+			ret.append("(");
 			break;
-		case 5:
-			return 3;
+		case RIGHT_PAREN:
+			ret.append("(");
 			break;
-		case 6:
-			return 1;
+		case END_OF_STRING:
+			ret.append("$");
 			break;
+		case E:
+			ret.append("E");
+			break;
+		case T:
+			ret.append("T");
+			break;
+		case F:
+			ret.append("F");
+			break;
+		default:
+			std::string out_string;
+			std::stringstream ss;
+			ss << k;
+			ret.append(ss.str());
 		}
+		temp.pop();
 	}
 
-	static int getLHS(Action action) {
+	string reverse;
 
-		int ruleNumber = action.GetRuleNumber();
+	int j = 0;
 
-		switch (ruleNumber) {
-		
-		case 1:
-			return E;
-			break;
-		case 2:
-			return E;
-			break;
-		case 3:
-			return T;
-			break;
-		case 4:
-			return T;
-			break;
-		case 5:
-			return F;
-			break;
-		case 6:
-			return F;
-			break;
-		}
+	for (int i = ret.length() - 1; i >= 0; i--) {
+		reverse.append(ret.substr(i, 1));
 	}
+	return reverse;
+}
 
-};
+string LRParser::PrintInput(int idx) {
+	return input.substr(idx, input.length() - idx);
+}
 
+string LRParser::PrintAction(Action action) {
+	return action.GetType() + " ";
+}
 
-class LRParser {
-private:
-	string input;
-	stack<int> parsingStack;
+Action LRParser::GetAction(int state, char c) {
 
-	Action actionTable[STATE_COUNT][TERMINAL_COUNT] = {
-		//0
-		Action("shift", 5), Action("error", ERROR), Action("error", ERROR), Action("shift", 4), Action("error", ERROR), Action("error", ERROR),
-		//1
-		Action("error", ERROR), Action("shift", 6), Action("error", ERROR), Action("error", ERROR), Action("error", ERROR), Action("accept", ACCEPT),
-		//2
-		Action("error", ERROR), Action("reduce", 2), Action("shift", 7), Action("error", ERROR), Action("reduce", 2),  Action("reduce", 2),
-		//3
-		Action("error", ERROR), Action("reduce",4), Action("reduce", 4), Action("error", ERROR), Action("reduce", 4), Action("reduce", 4),
-		//4
-		Action("shift", 5), Action("error", ERROR), Action("error", ERROR), Action("shift", 4), Action("error", ERROR), Action("error", ERROR),
-		//5
-		Action("error", ERROR), Action("reduce", 6), Action("reduce",6), Action("error", ERROR), Action("reduce",6), Action("reduce", 6),
-		//6
-		Action("shift", 5), Action("error", ERROR), Action("error", ERROR), Action("shift", 4), Action("error", ERROR), Action("error", ERROR),
-		//7
-		Action("shift", 5), Action("error", ERROR), Action("error", ERROR), Action("shift", 4), Action("error", ERROR), Action("error", ERROR),
-		//8
-		Action("error", ERROR), Action("shift", 6), Action("error", ERROR), Action("error", ERROR), Action("shift", 11), Action("error", ERROR),
-		//9
-		Action("error", ERROR), Action("reduce", 1), Action("shift", 7), Action("error", ERROR), Action("reduce", 1), Action("reduce", 1),
-		//10
-		Action("error", ERROR), Action("reduce", 3), Action("reduce", 3), Action("error", ERROR), Action("reduce", 3), Action("reduce", 3),
-		//11
-		Action("error", ERROR), Action("reduce", 5), Action("reduce", 5), Action("error", ERROR), Action("reduce", 5), Action("reduce", 5),
-	};
+	int symbol = GetSymbol(c);
 
-	const int gotoTable[STATE_COUNT][NONTERMINAL_COUNT] = {
-		1, 2, 3,
-		ERROR, ERROR, ERROR,
-		ERROR, ERROR, ERROR,
-		ERROR, ERROR, ERROR,
-		8, 2, 3,
-		ERROR, ERROR, ERROR,
-		ERROR, 9, 3,
-		ERROR, ERROR, 10,
-		ERROR, ERROR, ERROR,
-		ERROR, ERROR, ERROR,
-		ERROR, ERROR, ERROR,
-		ERROR, ERROR, ERROR,
-	};
-
-public:
-	
-	LRParser(string input) {
-		this->input = input;
+	if (symbol == ERROR) {
+		return Action("error", ERROR);
 	}
-
-	void Reduce(){
-
+	else {
+		return actionTable[state][symbol - ID];
 	}
+}
 
-	int GetSymbol(char c) {
-		switch (c) {
-		case 'a':
-			return ID;
-			break;
-		case '+':
-			return ADD;
-			break;
-		case '*':
-			return MUL;
-			break;
-		case '(':
-			return LEFT_PAREN;
-			break;
-		case ')':
-			return RIGHT_PAREN;
-			break;
-		case '\0':
-			return END_OF_STRING;
-		}
-		
-		return ERROR;
-	}
+void LRParser::Run() {
 
-	string PrintStack() {
-		stack <int> temp;
-		temp = parsingStack;
+	int i = 0;
+	int cur_state = 0;
 
-		string ret;
+	int step = 0;
 
-		while (!temp.empty()) {
-			int k = temp.top();
+	parsingStack.push(cur_state);
 
-			switch (k) {
-			case ID:
-				ret.append("a");
-				break;
-			case ADD:
-				ret.append("+");
-				break;
-			case MUL:
-				ret.append("*");
-				break;
-			case LEFT_PAREN:
-				ret.append("(");
-				break;
-			case RIGHT_PAREN:
-				ret.append("(");
-				break;
-			case END_OF_STRING:
-				ret.append("$");
-				break;
-			case E:
-				ret.append("E");
-				break;
-			case T:
-				ret.append("T");
-				break;
-			case F:
-				ret.append("F");
-				break;
-			default:
-				std::string out_string;
-				std::stringstream ss;
-				ss << k;
-				ret.append(ss.str());
-			}
-			temp.pop();
+	cout << "Stack" << "\t" << "Input" << "\t" << "Action" << endl;
+	while (true) {
+
+		int symbol = GetSymbol(input[i]);
+		Action action = GetAction(cur_state, input[i]);
+
+		if (action.GetType() == "shift") {
+
+			cur_state = action.GetState();
+
+			parsingStack.push(symbol);
+			parsingStack.push(action.GetState());
+
+			i++;
 		}
 
-		string reverse;
+		else if (action.GetType() == "reduce") {
 
-		int j = 0;
-		
-		for (int i = ret.length()-1; i >= 0; i--) {	
-			reverse.append(ret.substr(i, 1));
-		}
-		return reverse;
-	}
+			int k = Rule::GetRHSCount(action);
 
-	string PrintInput(int idx) {
-		return input.substr(idx, input.length() - idx);
-	}
-
-	string PrintAction(Action action) {
-		return action.GetType() + " ";
-	}
-
-	Action GetAction(int state, char c) {
-
-		int symbol = GetSymbol(c);
-
-		if (symbol == ERROR) {
-			return Action("error", ERROR);
-		}
-		else {
-			return actionTable[state][symbol-ID];
-		}
-	}
-
-	void Run() {
-
-		int i = 0;
-		int cur_state = 0;
-
-		int step = 0;
-
-		parsingStack.push(cur_state);
-
-			cout << "Stack" <<"\t" << "Input" << "\t" << "Action" <<  endl;
-		while (true) {
-
-			int symbol = GetSymbol(input[i]);
-			Action action = GetAction(cur_state, input[i]);
-
-			if (action.GetType() == "shift") {
-
-				cur_state = action.GetState();
-
-				parsingStack.push(symbol);
-				parsingStack.push(action.GetState());
-
-				i++;
+			for (int j = 0; j < k * 2; j++) {
+				parsingStack.pop();
 			}
 
-			else if (action.GetType() == "reduce") {
-
-				int k = Rule::getRHSCount(action);
-
-				for (int j = 0; j < k * 2; j++) {
-					parsingStack.pop();
-				}
-
-				cur_state = parsingStack.top();
-				int lhs = Rule::getLHS(action);
-				parsingStack.push(lhs);
+			cur_state = parsingStack.top();
+			int lhs = Rule::GetLHS(action);
+			parsingStack.push(lhs);
 
 			//	cout << "goto[" << cur_state  << "][" << lhs-E <<"]"<< endl; 
-				int new_state = gotoTable[cur_state][lhs-E];
-				parsingStack.push(new_state);
-				cur_state = new_state;
-			}
-
-			else if (action.GetType() == "accept") {
-				cur_state = ACCEPT;
-			}
-			else if (action.GetType() == "error") {
-				cur_state = ERROR;
-			}
-			else {
-				cur_state = ERROR;
-			}
-
-			cout << PrintStack() <<"\t" << PrintInput(i) << "\t" << PrintAction(action) << "\t" << action.GetState() << endl;
-			step++;
-			if (cur_state == ERROR) {
-				cout << "에러 스트링"<< endl;
-				break; 
-			}
-			
-			if (cur_state == ACCEPT) {
-				cout << "Accept!"<< endl;
-				break; 
-			}
-
+			int new_state = gotoTable[cur_state][lhs - E];
+			parsingStack.push(new_state);
+			cur_state = new_state;
 		}
-	}
 
-};
+		else if (action.GetType() == "accept") {
+			cur_state = ACCEPT;
+		}
+		else if (action.GetType() == "error") {
+			cur_state = ERROR;
+		}
+		else {
+			cur_state = ERROR;
+		}
+
+		cout << PrintStack() << "\t" << PrintInput(i) << "\t" << PrintAction(action) << "\t" << action.GetState() << endl;
+		step++;
+		if (cur_state == ERROR) {
+			cout << "에러 스트링" << endl;
+			break;
+		}
+
+		if (cur_state == ACCEPT) {
+			cout << "Accept!" << endl;
+			break;
+		}
+
+	}
+}
+
 
 int main() {
 
